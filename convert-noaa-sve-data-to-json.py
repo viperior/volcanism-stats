e5617e40b7dfdfafce1875aaf11636b7c5ff6cc8
viperior <viperior@gmail.com>
2019-09-25 01:49:32 +0000

    Modify CSV->JSON conversion script to use csv.DictReader().

--- convert-noaa-sve-data-to-json.py
+++ convert-noaa-sve-data-to-json.py
@@ -1,6 +1,17 @@
-import csv
+import csv, json
 
-with open('./volerup-noaa.txt', newline = '') as sve_data_file:                                                                                          
-    	sve_csv_data = csv.reader(sve_data_file, delimiter='\t')
-    	for sve in sve_csv_data:
-    		print(sve)
+csv_filename = './volerup-noaa.txt'
+output_json_filename = './volerup-noaa.json'
+
+with open(csv_filename, newline = '') as sve_data_file:                                                                                          
+  reader = csv.DictReader(sve_data_file, delimiter = '\t')
+  
+  data_dict = {}
+  
+  for index, current_dict in enumerate(reader):
+    data_dict[index] = current_dict
+  
+  output_dict = json.loads(json.dumps(data_dict))
+  
+  with open(output_json_filename, 'w') as output_file:
+    json.dump(output_dict, output_file)
